9b2e5a5acfd5ef8484cc6e4bb041017413e50b89
Dakota O'Neil <oneilkody@gmail.com>
2021-04-12 23:13:40 -0500

    add one solution

--- misc.js
+++ misc.js
@@ -995,3 +995,31 @@ function count(array){
 function greet(name) {
   return name ? `hello ${name}!` : null
 }
+
+
+
+
+// The old switcheroo
+
+// Write a function
+
+// vowel2index(str)
+// that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+// E.g:
+
+// vowel2index('this is my string') == 'th3s 6s my str15ng'
+// vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+// vowel2index('') == ''
+// Your function should be case insensitive to the vowels.
+
+function vowel2index(str) {
+  //code me
+ let arr = str.split('')
+ let vowels = 'aeiou'
+ for (let i = 0 ; i < str.length ; i++) {
+   if (vowels.includes(arr[i].toLowerCase())) {
+     arr.splice(i,1,i+1)
+   }
+ }
+ return arr.join('')
+}
