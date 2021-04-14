d0e38d577a9fcd4f8e698b928cdeb9004fd6bf30
Dakota O'Neil <oneilkody@gmail.com>
2021-04-13 23:17:12 -0500

    add one solution

--- misc.js
+++ misc.js
@@ -1023,3 +1023,21 @@ function vowel2index(str) {
  }
  return arr.join('')
 }
+
+
+
+
+
+// ASCII Total
+
+// You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all ASCII characters.
+
+// examples:
+
+// uniTotal("a") == 97 uniTotal("aaa") == 291
+
+function uniTotal(str)
+{
+// total up dem unicodes!
+  return str ? str.split('').map(el=>el.charCodeAt()).reduce((acc,c)=>acc+c) : 0
+}
