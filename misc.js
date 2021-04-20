bf08490fe5870ca0aad9b73b70a7c22a578a7e47
Dakota O'Neil <oneilkody@gmail.com>
2021-04-20 11:48:53 -0500

    first tryyyyyyyy

--- misc.js
+++ misc.js
@@ -129,3 +129,36 @@ function diamond(n){
   //Examples - n=1 returns '*\n', n=5 returns '   *\n ***\n*****\n ***\n  *\n'
   //Pseudo - first i tried to make it as a string but this time i'm gonna try to make it as
   //        an array and then return array.join('\n')
+
+
+
+
+
+
+//   Folding your way to the moon
+
+//   Have you heard about the myth that if you fold a paper enough times, you can reach the moon with it? Sure you have, but exactly how many? Maybe it's time to write a program to figure it out.
+
+//   You know that a piece of paper has a thickness of 0.0001m. Given distance in units of meters, calculate how many times you have to fold the paper to make the paper reach this distance.
+//   (If you're not familiar with the concept of folding a paper: Each fold doubles its total thickness.)
+  
+//   Note: Of course you can't do half a fold. You should know what this means ;P
+  
+//   Also, if somebody is giving you a negative distance, it's clearly bogus and you should yell at them by returning null (or whatever equivalent in your language. In Shell please return None).
+
+function foldTo(distance) {
+    let foldCount = 0
+    let thickness = 0.0001
+    while (thickness < distance) {
+      foldCount++
+      thickness*=2
+    }
+    return distance >= 0 ? foldCount : null
+  }
+  
+  /*
+  declare a foldCount variable and assign it to 0
+  declare a thickness variable and assign it to 0.0001
+  run a while loop that will double the thickness every loop and increase the foldCount by one
+      until the thickness is >= distance
+  */
