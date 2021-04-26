78119038202ae2f40e2687d0984da37e539907b0
Dakota O'Neil <oneilkody@gmail.com>
2021-04-26 18:07:52 -0500

    add the codewar to todays dailystandup

--- misc.js
+++ misc.js
@@ -181,3 +181,16 @@ function foldTo(distance) {
 function howManyTimes(arr1, arr2) {
   return arr2.map(x=> arr1.filter(y=>y==x).length)
 }
+
+
+
+
+// Remove consecutive duplicate words
+
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+const removeConsecutiveDuplicates = s => s.split(' ').filter((el,i,arr)=> el != arr[i+1]).join(' ')
