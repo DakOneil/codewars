c7a1500af572b7cd98b53a290466a2d28562e1a7
Dakota O'Neil <oneilkody@gmail.com>
2021-04-29 22:26:30 -0500

    add one sol

--- misc.js
+++ misc.js
@@ -911,3 +911,21 @@ function toLeetSpeak(str) {
   }
   return finalStr
 }
+
+
+
+
+// Reverse every other word in the string
+
+// Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+
+function reverse(str){
+  //WRITE SOME MAGIC
+  let words = str.split(' ')
+  for (let i = 0 ; i < words.length ; i++) {
+    if (i % 2 != 0) {
+      words[i] = words[i].split('').reverse().join('')
+    }
+  }
+  return words.join(' ').trim()
+}
