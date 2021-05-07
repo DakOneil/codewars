c810a08c8cc882d0165335d1951f8c5de29fd74f
Dakota O'Neil <oneilkody@gmail.com>
2021-05-07 07:48:15 -0500

    add another cw solution

--- 5-7-21.js
+++ 5-7-21.js
@@ -48,3 +48,29 @@ function towerBuilder(nFloors) {
     
     return arr
   }
+
+//   Count number of zeros from 1 to N
+
+//   Create an algorithm to count the number of zeros that appear between 1 and N.
+
+// Examples:
+
+// There are 2 zeros from 1 to 20: 10, 20
+
+// There are 11 zeros from 1 to 100: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+
+// countZeros(10); // returns 1
+// countZeros(100); // returns 11
+// countZeros(200); // returns 31
+
+function countZeros(n) {
+  // code here
+  let count = 0
+  for (let i = 1 ; i <= n ; i++) {
+    let letter = i.toString()
+    for (let j = 0 ; j < letter.length ; j++) {
+      if (letter[j]=='0') count++
+    }
+  }
+  return count
+}
