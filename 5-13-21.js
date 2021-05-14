5fc092989cd9d21bed850fd1fc16cf3e7d74b7f0
Dakota O'Neil <oneilkody@gmail.com>
2021-05-13 22:18:36 -0500

    add one more solution

--- 5-13-21.js
+++ 5-13-21.js
@@ -94,3 +94,22 @@ function symmetricPoint(p, q) {
 
 
 
+
+// Calculate Price Excluding VAT
+
+// Write a function that calculates the original product price, without VAT.
+
+// Example
+// If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
+
+// Thus, if your function receives 230.00 as input, it should return 200.00
+
+// Notes:
+
+// VAT is always 15% for the purposes of this Kata.
+
+//return price without vat
+function excludingVatPrice(price){
+  // your code
+  return Number((0.86956521739*price).toFixed(2));
+}
