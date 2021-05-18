7b1f008d88a34b1861c1f0da4ea55d3b3e37f4ab
Dakota O'Neil <oneilkody@gmail.com>
2021-05-18 07:26:33 -0500

    add one more sol

--- 5-18-21.js
+++ 5-18-21.js
@@ -71,3 +71,25 @@ function first(arr, n) {
     if (!n) n=1
     return arr.slice(0,n)
   }
+
+
+
+
+//   pick a set of first elements
+
+//   Write a function to get the first elements of asequence. Passing a parameter n (default=1) will return the first n elements of the sequence.
+
+// If n == 0 return an empty sequence []
+
+// Examples
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+
+  function first(arr, n) {
+    if (typeof n == 'number') {
+      return arr.slice(0,n)
+    } else return [arr[0]]
+  }
