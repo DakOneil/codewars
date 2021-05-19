3775e1793ac229fa0bbfbfbd32087af611cfac58
Dakota O'Neil <oneilkody@gmail.com>
2021-05-18 22:29:50 -0500

    push to older date

--- misc.js
+++ misc.js
@@ -43,3 +43,33 @@ function minSum(arr) {
   sum the products
   return the sum
   */
+
+
+
+
+
+//   Sum of array singles
+
+//   In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+// More examples in the test cases.
+
+// Good luck!
+
+function repeats(arr){
+    //..
+      let singles = []
+      for (let i = 0 ; i < arr.length ; i++) {
+        let check = arr[i]
+        let checkArr = []
+        for (let j = 0 ; j < arr.length ; j++) {
+          if (arr[j] == arr[i]) {
+            checkArr.push(arr[j])
+          }
+        }
+        if (checkArr.length == 1) singles.push(checkArr[0])
+      }
+      return singles.reduce((acc,c)=>acc+c)
+    };
