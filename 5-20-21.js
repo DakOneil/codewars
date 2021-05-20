80ac3706963ed5e9f340751329514f6b9e70ff8a
Dakota O'Neil <oneilkody@gmail.com>
2021-05-20 16:24:47 -0500

    add solution

--- 5-20-21.js
+++ 5-20-21.js
@@ -86,3 +86,25 @@ function getStatus(isBusy) {
   }
   return state
 }
+
+
+
+// Broken Counter
+
+// Our counter prototype is broken. Can you spot, what's wrong here?
+
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase = function() {
+  this.value++;
+};
+
+Counter.prototype.getValue = function() {
+  return this.value;
+};
+
+Counter.prototype.reset = function() {
+  this.value = 0;
+};
