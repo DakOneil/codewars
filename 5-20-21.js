6936223e5940abebbe7a348f49cc097adf54813d
Dakota O'Neil <oneilkody@gmail.com>
2021-05-20 16:12:29 -0500

    add one sol

--- 5-20-21.js
+++ 5-20-21.js
@@ -21,3 +21,28 @@ function whatNumberIsIt(n){
     else if (n == -Number.MAX_VALUE*2) return 'Input number is Number.NEGATIVE_INFINITY'
     else return `Input number is ${n}`
   }
+
+
+
+
+
+//   Closest elevator
+
+//   Given 2 elevators (named "left" and "right") in a building with 3 floors (numbered 0 to 2), write a function elevator accepting 3 arguments (in order):
+
+// left - The current floor of the left elevator
+// right - The current floor of the right elevator
+// call - The floor that called an elevator
+// It should return the name of the elevator closest to the called floor ("left"/"right").
+
+// In the case where both elevators are equally distant from the called floor, choose the elevator to the right.
+
+// You can assume that the inputs will always be valid integers between 0-2.
+
+function elevator(left, right, call){
+  // code on! :)
+  let leftDis = Math.abs(call-left)
+  let rightDis = Math.abs(call-right)
+  return leftDis < rightDis ? 'left' : 'right'
+}
+
