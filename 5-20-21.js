b81a04601074c5b944b5b7276bd7007509f5805b
Dakota O'Neil <oneilkody@gmail.com>
2021-05-20 17:15:22 -0500

    add sol but i think its wrong

--- 5-20-21.js
+++ 5-20-21.js
@@ -134,3 +134,24 @@ Counter.prototype.reset = function() {
     }
     return arr.join('\n')
   }
+
+
+
+
+  // Shifty Closures
+
+  // Functional closures can get overly attached. Set them straight!
+
+// Why doesn't greet_abe() actually greet Abe?
+
+var greet_abe = function() {
+  var name = 'Abe';
+  return "Hello, " + name + '!';
+};
+
+var greet_ben = function() {
+  var name = 'Ben';
+  return "Hello, " + name + '!';
+};
+
+
