943789131da0a7df162fa54db57fd764f8275a63
Dakota O'Neil <oneilkody@gmail.com>
2021-05-24 07:27:07 -0500

    add one solution

--- 5-24-21.js
+++ 5-24-21.js
@@ -31,12 +31,25 @@ function Warrior(n){
 
 //   Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
 
-  function logs(x , a, b){
-    //your code here
-      return Math.log(a)/Math.log(x) + Math.log(b)/Math.log(x) 
-    }
+function logs(x , a, b){
+//your code here
+  return Math.log(a)/Math.log(x) + Math.log(b)/Math.log(x) 
+}
 
 
 
 
 
+// Generate user links
+
+// Your task is to create userlinks for the url, you will be given a username and must return a valid link.
+
+// Example
+// generate_link('matt c')
+// http://www.codewars.com/users/matt%20c
+
+function generateLink(user) {
+    const homeLink = 'http://www.codewars.com/users/'
+    let userLink = encodeURIComponent(user)
+    return `${homeLink}${userLink}`
+  }
