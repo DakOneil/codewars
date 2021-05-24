7b25e56a0bf6e4ad2f17ae959345d9bf78ceb7b6
Dakota O'Neil <oneilkody@gmail.com>
2021-05-23 23:32:03 -0500

    edit solution

--- 5-23-21.js
+++ 5-23-21.js
@@ -28,6 +28,7 @@ function dBScale(intensity) {
 //   Task
 // Provided is a function find which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 85) You may assume that all array elements are unique.
 
-function find(array, element) {
-    return array.indexOf(element) > -1 ? array.indexOf(element) : "Not found";
+function find(ar, e) {
+    let a = ar.indexOf(e)
+    return a>-1?a:"Not found";
   }
