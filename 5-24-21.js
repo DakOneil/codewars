3f7fd2e5dc2b4191491d4732a6f3741ce88991a8
Dakota O'Neil <oneilkody@gmail.com>
2021-05-24 21:19:04 -0500

    add a sol

--- 5-24-21.js
+++ 5-24-21.js
@@ -89,3 +89,52 @@ function factorial(n){
   }
   return fac == 0 ? 1 : fac
 }
+
+
+
+
+// Alphabet war
+
+// Introduction
+// There is a war and nobody knows - the alphabet war!
+// There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+
+// Task
+// Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+// The left side letters and their power:
+
+//  w - 4
+//  p - 3
+//  b - 2
+//  s - 1
+// The right side letters and their power:
+
+//  m - 4
+//  q - 3
+//  d - 2
+//  z - 1
+// The other letters don't have power and are only victims.
+
+function alphabetWar(fight)
+{
+  const leftObj = {
+    'w': 4, 
+    'p': 3, 
+    'b': 2, 
+    's': 1
+  }
+  const rightObj = {
+    'm': 4, 
+    'q': 3, 
+    'd': 2, 
+    'z': 1
+  }
+  let left = 0
+  let right = 0
+  for (let i = 0 ; i < fight.length ; i++) {
+    if (Object.keys(leftObj).includes(fight[i])) left += leftObj[fight[i]]
+    else if (Object.keys(rightObj).includes(fight[i])) right += rightObj[fight[i]]
+  }
+   return left > right ? 'Left side wins!' : right > left ? 'Right side wins!' : "Let's fight again!";
+}
