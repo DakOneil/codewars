faa6d19124b2271efb8936b82b72ac93b618f75f
Dakota O'Neil <oneilkody@gmail.com>
2021-05-26 00:14:35 -0500

    add sol to 5-25-21.js

--- 5-25-21.js
+++ 5-25-21.js
@@ -293,3 +293,29 @@ for (let i = 0 ; i < strarr.length-(k-1) ; i++) {
 }
 return str
 }
+
+
+
+
+// Rot13
+
+// ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+// Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+function rot13(message){
+  //your code here
+  let ab = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+  let AB = ab.toUpperCase()
+  let str = ''
+  for (let i = 0 ; i < message.length ; i++ ){
+    if (ab.includes(message[i])) {
+      str += ab[ab.indexOf(message[i])+13]
+    } else if (AB.includes(message[i])) {
+      str += AB[AB.indexOf(message[i])+13]
+    } else {
+      str += message[i]
+    }
+  }
+  return str
+}
