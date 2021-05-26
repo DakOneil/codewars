32cea61beeb3d85e88d329940a0ff300677af085
Dakota O'Neil <oneilkody@gmail.com>
2021-05-25 23:03:30 -0500

    add sol to 5-25-21.js

--- 5-25-21.js
+++ 5-25-21.js
@@ -164,3 +164,44 @@ function tickets(peopleInLine){
 // run a loop to add in the counts
 // run a loop that checks each peopleInLine value -25
 // to see if it can return the correct bills
+
+
+
+
+
+// Delete occurrences of an element if it occurs more than n times
+
+// Enough is enough!
+// Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motive usually repeats. He isn't fond of seeing the Eiffel tower 40 times. He tells them that he will only sit during the session if they show the same motive at most N times. Luckily, Alice and Bob are able to encode the motive as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+
+// Task
+// Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+
+// Example
+//   deleteNth ([1,1,1,1],2) // return [1,1]
+  
+//   deleteNth ([20,37,20,21],1) // return [20,37,21]
+
+function deleteNth(arr,n){
+  // ...
+  let obj = {}
+  let newArr = []
+  for (let i = 0 ; i < arr.length ; i++) {
+    if (!obj[arr[i]]) {
+      obj[arr[i]] = 1;
+      newArr.push(arr[i])
+    }
+    else if (obj[arr[i]] < n) {
+      obj[arr[i]]++;
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
+}
+// P: an arr of nums and a num that is the max count
+// R: an array from arr that contains the same nums minus any extra
+// E:
+// P: create an object 
+// loop through the array adding the number and its count to the object
+// adding each arr[i] to the new array until the count value is equal to N
+// return new array
