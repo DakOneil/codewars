75639b7da9acd7b1f76c17e6540243ce00538062
Dakota O'Neil <oneilkody@gmail.com>
2021-06-03 06:57:51 -0500

    add 6-3-21.js

---
+++ 6-3-21.js
@@ -0,0 +1,40 @@
+// Polish alphabet
+
+// There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+// Your task is to change the letters with diacritics:
+
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+// and print out the string without the use of the Polish letters.
+
+// For example:
+
+// "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+function correctPolishLetters (string) {
+    // your code
+    let arr = string.split('')
+    let polishObj = {
+      'ą': 'a',
+      'ć': 'c',
+      'ę': 'e',
+      'ł': 'l',
+      'ń': 'n',
+      'ó': 'o',
+      'ś': 's',
+      'ź': 'z',
+      'ż': 'z'
+    }
+    for (let i = 0 ; i < arr.length ; i++) {
+      if (arr[i] in polishObj) arr.splice(i,1,polishObj[arr[i]])
+    }
+    return arr.join('')
+  }
