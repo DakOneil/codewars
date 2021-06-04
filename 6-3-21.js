6383204567f1f5e7e09a91a24a212ac191e591d8
Dakota O'Neil <oneilkody@gmail.com>
2021-06-03 21:57:02 -0500

    add solution

--- 6-3-21.js
+++ 6-3-21.js
@@ -38,3 +38,20 @@ function correctPolishLetters (string) {
     }
     return arr.join('')
   }
+
+
+
+
+//   All unique
+
+//   Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+
+function hasUniqueChars(str){
+    // ...
+    for (let i = 0 ; i < str.length ; i++) {
+      if (str.includes(str[i], i+1)) {return false}
+    }
+    return true
+  }
