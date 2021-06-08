4424f4ec0efaaf7679a589d8ad6494c6fd5428c0
Dakota O'Neil <oneilkody@gmail.com>
2021-06-08 15:09:21 -0500

    add a sol to 6-8-21.js

--- 6-8-21.js
+++ 6-8-21.js
@@ -19,3 +19,28 @@ function lowercaseCount(str){
     //How many?
   return (str.match(/[a-z]/g) == null) ? 0 : str.match(/[a-z]/g).length
 }
+
+
+
+
+// Swap Values
+
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+function swapValues(a) {
+    return a.reverse()
+  }
+
+
+
+
+//   Hex to Decimal
+
+//   Complete the function which converts hex number (given as a string) to a decimal number.
+  
+  function hexToDec(hexString){
+    //your code here
+    return parseInt(hexString,16)
+  }
