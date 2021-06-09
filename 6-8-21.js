848823e26e92525b0a39b54b767f1a786765ec5f
Dakota O'Neil <oneilkody@gmail.com>
2021-06-08 22:08:45 -0500

    add solution

--- 6-8-21.js
+++ 6-8-21.js
@@ -93,3 +93,18 @@ class Solution{
 function validateUsr(username) {
   return /^[a-z0-9_]{4,16}$/.test(username)
 }
+
+
+
+// validate code with simple regex
+
+// Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+
+// You can assume the input will always be a number.
+
+//FIRST TRYYYYY
+function validateCode (code) {
+  //your code here
+    return /^[1-3]/.test(code)
+  }
+
