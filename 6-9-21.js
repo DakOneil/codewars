707a411e1f434b6148bc6c9a8667383a0600166d
Dakota O'Neil <oneilkody@gmail.com>
2021-06-09 22:07:44 -0500

    add a solution

--- 6-9-21.js
+++ 6-9-21.js
@@ -65,3 +65,32 @@ function checkCoupon(eCode, cCode, currentDate, expirationDate){
     if (eCode === cCode && eDate >= cDate) return true
     else return false
   }
+
+
+
+
+// Implement Array.prototype.filter()
+
+// What we want to implement is Array.prototype.filter() function, just like the existing Array.prototype.filter(). Another similar function is _.filter() in underscore.js and lodash.js.
+
+// The usage will be quite simple, like:
+
+// [1, 2, 3, 4, 5].filter(num => num > 3) == [4, 5]
+// Of course, the existing Array.prototype.filter() function has been undefined for the purposes of this Kata.
+
+Array.prototype.filter = function (func) {
+  //nothing here, you will feel pretty awesome to write down by yourself
+  //have fun :)
+  let arr = []
+  for (let i =0 ; i < this.length ;i++ ){
+    if(func(this[i])) {
+      arr.push(this[i])
+    }
+  }
+  return arr
+}
+
+
+
+
+
