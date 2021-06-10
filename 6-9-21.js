0e74a69a38820d6fd6978f74db2ec5da6bcea856
Dakota O'Neil <oneilkody@gmail.com>
2021-06-09 22:10:01 -0500

    add a solution

--- 6-9-21.js
+++ 6-9-21.js
@@ -93,4 +93,12 @@ Array.prototype.filter = function (func) {
 
 
 
+//FIRST TRYYYYYYY
+// Enumerable Magic #1 - True for All?
 
+// Create a method all which takes an array and a predicate (function pointer), and returns true if the predicate returns true for every element in the array. Otherwise, it should return false. If the array is empty, it should return true, since technically nothing failed the test.
+
+function all( arr, fun ){
+  // ...
+  return arr.filter(n=>fun(n)).length == arr.length
+}
