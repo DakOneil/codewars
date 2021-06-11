c8c651c4425be301790fc318ca790bb0f01f1570
Dakota O'Neil <oneilkody@gmail.com>
2021-06-10 21:54:29 -0500

    add solution

--- 6-10-21.js
+++ 6-10-21.js
@@ -38,3 +38,38 @@ function rgb(r, g, b){
     if (b.length ==1) b = 0 + b
     return (r + g + b).toUpperCase()
   }
+
+
+
+
+//   Find the missing letter
+
+//   #Find the missing letter
+
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+
+// ['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+
+// ["a","b","c","d","f"] -> "e"
+// ["O","Q","R","S"] -> "P"
+// (Use the English alphabet with 26 letters!)
+
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+// I have also created other katas. Take a look if you enjoyed this kata!
+
+function findMissingLetter(array){
+    let aBet = 'abcdefghijklmnopqrstuvwxyz'
+    for (let i = 0 ; i < array.length-1 ; i++) {
+      let thisOne = aBet.indexOf(array[i].toLowerCase())
+      let nextOne = aBet[thisOne+1]
+      if (array[i+1].toLowerCase() != nextOne) {
+        return array[i] == aBet[thisOne] ? nextOne : nextOne.toUpperCase()
+      }
+    }
+  }
