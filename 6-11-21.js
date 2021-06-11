42866cfbcb93a3241c77e26bf12b8850c36bdca0
Dakota O'Neil <oneilkody@gmail.com>
2021-06-11 15:56:37 -0500

    add sol

--- 6-11-21.js
+++ 6-11-21.js
@@ -53,3 +53,33 @@ function fiveLine(s){
     s = s.trim()
     return `${s}\n${s}${s}\n${s.repeat(3)}\n${s.repeat(4)}\n${s.repeat(5)}`
   }
+
+
+
+
+//   Directions Reduction
+
+//   Task
+// Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
+// The Haskell version takes a list of directions with data Direction = North | East | West | South.
+// The Clojure version returns nil when the path is reduced to nothing.
+// The Rust version takes a slice of enum Direction {North, East, West, South}.
+// See more examples in "Sample Tests:"
+// Notes
+// Not all paths can be made simpler. The path ["NORTH", "WEST", "SOUTH", "EAST"] is not reducible. "NORTH" and "WEST", "WEST" and "SOUTH", "SOUTH" and "EAST" are not directly opposite of each other and can't become such. Hence the result path is itself : ["NORTH", "WEST", "SOUTH", "EAST"].
+// if you want to translate, please ask before translating.
+
+function dirReduc(arr){
+  // ...
+  for (let i = 0 ; i < arr.length ; i++) {
+    if ((arr[i]=='NORTH' && arr[i+1]=='SOUTH') ||
+        (arr[i]=='SOUTH' && arr[i+1]=='NORTH') ||
+        (arr[i]=='WEST' && arr[i+1]=='EAST') ||
+        (arr[i]=='EAST' && arr[i+1]=='WEST')) {
+      arr.splice(i,2);
+      i=-1;
+    }
+  }
+  return arr
+}
