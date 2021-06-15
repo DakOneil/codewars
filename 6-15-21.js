c6f03732e7d25c7370e8b6dc0df80c96113aaa22
Dakota O'Neil <oneilkody@gmail.com>
2021-06-15 09:49:11 -0500

    add sol

--- 6-15-21.js
+++ 6-15-21.js
@@ -8,3 +8,14 @@ function isToday(date) {
     //Code goes here.
     return date.toDateString() == new Date().toDateString()
   }
+
+
+
+//   Enumerable Magic #4 - True for None?
+
+//   Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
+
+function none(arr, fun){
+    // ...
+    return !arr.some(fun)
+  }
