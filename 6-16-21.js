bde810200d7a7ce301df23f9747a3ae18f488bdb
Dakota O'Neil <oneilkody@gmail.com>
2021-06-16 07:27:53 -0500

    add a solution

---
+++ 6-16-21.js
@@ -0,0 +1,74 @@
+// Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer
+
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+
+// Your task is to return one of the following strings:
+
+// < firstName here >, < country here > of the first Python developer who has signed up; or
+// There will be no Python developers if no Python developer has signed up.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+//   { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+// ];
+// your function should return Victoria, Puerto Rico.
+
+// Notes:
+
+// The input array will always be valid and formatted as in the example above.
+
+function getFirstPython(list) {
+    // Thank you for checking out my kata :)
+    function findPy(obj) {
+      return obj.language == 'Python'
+    }
+    let ind = list.findIndex(findPy)
+    if (ind>-1) {
+      return `${list[ind].firstName}, ${list[ind].country}`  
+    } else {
+      return "There will be no Python developers"
+    }
+  }
+
+
+
+
+
+//   Coding Meetup #5 - Higher-Order Functions Series - Prepare the count of languages
+
+//   You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return an object (associative array in PHP) which includes the count of each coding language represented at the meetup.
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+//   { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+//   { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+//   { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+// ];
+// your function should return the following object (associative array in PHP):
+
+// { C: 2, JavaScript: 1, Ruby: 1 }
+// Notes:
+
+// The order of the languages in the object does not matter.
+// The count value should be a valid number.
+// The input array will always be valid and formatted as in the example above.
+
+function countLanguages(list) {
+    // thank you for checking out the Coding Meetup kata :)
+    let langs = {}
+    for (let i = 0 ; i < list.length ; i++) {
+      let a = list[i].language
+      if (!langs[a]){
+        langs[a] = 1
+      } else {
+        langs[a]++
+      } 
+    }
+    return langs
+  }
