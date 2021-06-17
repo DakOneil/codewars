8ddadbcc3856ab3c15f017b963de27bafe7567c3
Dakota O'Neil <oneilkody@gmail.com>
2021-06-17 15:34:53 -0500

    add a solution

--- 6-17-21.js
+++ 6-17-21.js
@@ -121,3 +121,44 @@ function orderFood(list) {
     })
     return orders
   }
+
+
+
+
+//   Coding Meetup #15 - Higher-Order Functions Series - Find the odd names
+
+//   You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Given the following input array:
+
+// var list1 = [
+//   { firstName: 'Aba', lastName: 'N.', country: 'Ghana', continent: 'Africa', age: 21, language: 'Python' },
+//   { firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
+// ];
+// write a function that when executed as findOddNames(list1) returns only the developers where if you add the ASCII representation of all characters in their first names, the result will be an odd number:
+
+// [
+//   { firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
+// ]
+// Explanation of the above:
+
+// Sum of ASCII codes of letters in 'Aba' is: 65 + 98 + 97 = 260 which is an even number
+// Sum of ASCII codes of letters in 'Abb' is: 65 + 98 + 98 = 261 which is an odd number
+// Notes:
+
+// Preserve the order of the original list.
+// Return an empty array [] if there is no developer with an "odd" name.
+// The input array and first names will always be valid and formatted as in the example above.
+
+function findOddNames(list) {
+    // thank you for checking out the Coding Meetup kata :)
+    let oddNames = []
+    list.forEach(person=>{
+      let firstSum = 0
+      for (let i = 0 ; i < person.firstName.length ; i++) {
+        firstSum += person.firstName.charCodeAt(i)
+      }
+      if (firstSum % 2 != 0) oddNames.push(person)
+    })
+    return oddNames
+  }
