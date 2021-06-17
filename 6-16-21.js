c4da39c1244534224a191d26f9e2ba8cdcc04133
Dakota O'Neil <oneilkody@gmail.com>
2021-06-16 22:02:13 -0500

    add a solution

--- 6-16-21.js
+++ 6-16-21.js
@@ -178,3 +178,59 @@ function allContinents(list) {
     })
     return countries.includes('Africa' && 'Americas' && 'Asia' && 'Europe' && 'Oceania')
   }
+
+
+
+
+
+
+//   Coding Meetup #9 - Higher-Order Functions Series - Is the meetup age-diverse?
+
+//   You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return:
+
+// true if developers from all of the following age groups have signed up: teens, twenties, thirties, forties, fifties, sixties, seventies, eighties, nineties, centenarian (at least 100 years young).
+// false otherwise.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' },
+//   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+//   { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+//   { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
+//   { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
+//   { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
+//   { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
+//   { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
+//   { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript' }
+// ];
+// your function should return true as there is at least one developer from each age group.
+
+// Notes:
+
+// The input array will always be valid and formatted as in the example above.
+// Age is represented by a number which can be any positive integer up to 199.
+
+function isAgeDiverse(list) {
+  // thank you for checking out the Coding Meetup kata :)
+  let teens = num => num>12&&num<20
+  let twens = num => num>19&&num<30
+  let thirs = num => num>29&&num<40
+  let fors = num => num>39&&num<50
+  let fifs = num => num>49&&num<60
+  let sixs = num => num>59&&num<70
+  let sevs = num => num>69&&num<80
+  let eights = num => num>79&&num<90
+  let nins = num => num>89&&num<100
+  let cents = num => num>99&&num<200
+  let ages = []
+  list.forEach(person=> {
+    ages.push(person.age)
+  })
+  
+  if (ages.length < 10) return false
+  else if (!ages.some(teens) || !ages.some(twens) || !ages.some(thirs) || !ages.some(fors) || !ages.some(fifs) || !ages.some(sixs) || !ages.some(sevs) || !ages.some(eights) || !ages.some(nins) || !ages.some(cents)) return false
+  else return true
+}
