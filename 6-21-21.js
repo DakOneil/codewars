f10bfa18ff9d75900e5b82a50945271840101c6c
Dakota O'Neil <oneilkody@gmail.com>
2021-06-21 08:12:24 -0500

    add a solution

--- 6-21-21.js
+++ 6-21-21.js
@@ -48,3 +48,25 @@ function threeInOne(arr){
     }
     return sums
   }
+
+
+
+
+//   Training JS #22: Unlock new skills--Arrow function,spread operator and deconstruction
+
+//   Task
+// Create a function shuffleIt. The function accepts two or more parameters. The first parameter arr is an array of numbers, followed by an arbitrary number of numeric arrays. Each numeric array contains two numbers, which are indices for elements in arr (the numbers will always be within bounds). For every such array, swap the elements. Try to use all your new skills: arrow functions, the spread operator, destructuring, and rest parameters.
+
+// Example:
+
+// shuffleIt([1,2,3,4,5],[1,2]) should return [1,3,2,4,5]
+// shuffleIt([1,2,3,4,5],[1,2],[3,4]) should return [1,3,2,5,4]
+// shuffleIt([1,2,3,4,5],[1,2],[3,4],[2,3]) should return [1,3,5,2,4]
+
+function shuffleIt(arr,...a){
+    //coding here...
+    for (let i = 0 ; i < a.length ; i++) {
+      [arr[a[i][0]], arr[a[i][1]]] = [arr[a[i][1]], arr[a[i][0]]]
+    }
+    return arr
+  }
