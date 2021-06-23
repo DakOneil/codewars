77ea1206506d14383b06f550449cf2e9bb7e2db5
Dakota O'Neil <oneilkody@gmail.com>
2021-06-23 12:32:13 -0500

    add a solution

--- 6-23-21.js
+++ 6-23-21.js
@@ -27,3 +27,20 @@ function anyArrows(arrows){
 // a == !a
 
 const a = []
+
+
+
+
+
+// Points of Reflection
+
+// Task
+// Given two points P and Q, output the symmetric point of point P about Q. Each argument is a two-element array of integers representing the point's X and Y coordinates. Output should be in the same format, giving the X and Y coordinates of point P1. You do not have to validate the input.
+
+// This kata was inspired by the Hackerrank challenge Find Point
+
+function symmetricPoint(p, q) {
+  let a = p[0] - q[0]
+  let b = p[1] - q[1]
+  return [q[0]-a, q[1]-b]
+}
