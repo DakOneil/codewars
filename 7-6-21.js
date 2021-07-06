d1d430fe0256c423b0e09cff2be07ac9d5b17cd9
Dakota O'Neil <oneilkody@gmail.com>
2021-07-06 08:23:48 -0500

    add a solution

--- 7-6-21.js
+++ 7-6-21.js
@@ -53,3 +53,30 @@ function inAscOrder(arr) {
     }
     return true
   }
+
+
+
+
+
+
+// Simple Fun #176: Reverse Letter
+
+// Task
+// Given a string str, reverse it omitting all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+    //coding and coding..
+    str = str.split('')
+    return str.filter(el=> el.match(/^[a-zA-Z]+$/)).reverse().join('')
+  }
