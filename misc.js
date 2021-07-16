4a1414fbf5589d89ea484a004565b1a47fa5d3e9
Dakota O'Neil <oneilkody@gmail.com>
2021-07-16 12:06:58 -0500

    add a solution to misc

--- misc.js
+++ misc.js
@@ -391,3 +391,27 @@ function catMouse(x){
   let diff = Math.abs(mPos - cPos) - 1
   return diff > 3 ? "Escaped!" : "Caught!"
 }
+
+
+
+
+
+// Sum of Cubes
+
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+
+// Assume that the input n will always be a positive integer.
+
+// Examples:
+
+// sumCubes(2) // 9
+// sum of the cubes of 1 and 2 is 1 + 8
+
+function sumCubes(n){
+  // ...
+  let arr = []
+  for (let i = 1 ; i <= n ; i++) {
+    arr.push(i)
+  }
+  return arr.reduce((acc, c)=> acc + c**3, 0)
+}
