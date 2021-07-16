61d795b85f0e55d57c91df8fc3717a1bf8283a89
Dakota O'Neil <oneilkody@gmail.com>
2021-07-16 08:02:27 -0500

    add a solution to misc

--- misc.js
+++ misc.js
@@ -371,3 +371,23 @@ function divCon(x){
 function bump(x) {
   return !x.match(/n/g) ? "Woohoo!" : x.match(/n/g).length <= 15 ? "Woohoo!" : "Car Dead"
 }
+
+
+
+
+// Cat and Mouse - Easy Version
+
+// You will be given a string (x) featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'.
+
+// You need to find out if the cat can catch the mouse from it's current position. The cat can jump over three characters. So:
+
+// C.....m returns 'Escaped!' <-- more than three characters between
+
+// C...m returns 'Caught!' <-- as there are three characters between the two, the cat can jump.
+
+function catMouse(x){
+  let mPos = x.indexOf('m')
+  let cPos = x.indexOf('C')
+  let diff = Math.abs(mPos - cPos) - 1
+  return diff > 3 ? "Escaped!" : "Caught!"
+}
