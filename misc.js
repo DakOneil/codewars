d4758fafdd0c7b2366226c19c37f1195d5cf3631
Dakota O'Neil <oneilkody@gmail.com>
2021-07-16 07:55:01 -0500

    add a solution to misc

--- misc.js
+++ misc.js
@@ -339,3 +339,26 @@ function jumpingNumber(n){
   if (diffs.every(x=>x==1)) return "Jumping!!"
   else return "Not!!"
 }
+
+
+
+
+
+// Divide and Conquer
+
+// Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers.
+
+// Return as a number.
+
+function divCon(x){
+  let stringTotal = 0
+  let numTotal = 0
+  for (let i = 0 ; i < x.length ; i++) {
+    if (typeof x[i] == 'string') stringTotal += Number(x[i])
+    else numTotal += x[i]
+  }
+  return numTotal - stringTotal
+}
+
+
+
