ceaaac687b952e96374e379f20e7c78e8b2af2cb
Dakota O'Neil <oneilkody@gmail.com>
2021-07-20 07:39:56 -0500

    add sol for today

---
+++ 7-20-21.js
@@ -0,0 +1,15 @@
+// Squares sequence
+
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+// Examples
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+function squares(x, n) {
+    let arr = [x]
+    for (let i = 0 ; i < n -1 ; i++) {
+      arr.push(arr[i]**2)
+    }
+    return n > 0 ? arr : []
+  }
