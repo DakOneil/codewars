0dcca7b1e13fc2aacc1319e8a97d53bec2e6b274
Dakota O'Neil <oneilkody@gmail.com>
2021-07-19 22:46:52 -0500

    4kyu solution

--- misc.js
+++ misc.js
@@ -242,3 +242,42 @@ function expandedForm(num) {
   }
   return arr.join(' + ')
 }
+
+
+
+
+
+// Strip Comments
+
+// Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+
+// Example:
+
+// Given an input string of:
+
+// apples, pears # and bananas
+// grapes
+// bananas !apples
+// The output expected would be:
+
+// apples, pears
+// grapes
+// bananas
+// The code would be called like so:
+
+// var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+// // result should == "apples, pears\ngrapes\nbananas"
+
+function solution(input, markers) {
+  let strArr = input.split('\n')
+  let stripped = strArr.map(el=> {
+    let ind 
+    for (let i = 0; i < markers.length ; i++) {
+      if (el.includes(markers[i])) {
+        ind = el.indexOf(markers[i])
+      }
+    }
+    return el = el.slice(0,ind).trim()
+  })
+  return stripped.join('\n')
+};
