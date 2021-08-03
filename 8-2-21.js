50d37f43b384d121aed304c2aa3b7bcb1ddd4a04
Dakota O'Neil <oneilkody@gmail.com>
2021-08-02 21:17:02 -0500

    add solution for 8-2

---
+++ 8-2-21.js
@@ -0,0 +1,53 @@
+// The Vowel Code
+
+// Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+// a -> 1
+// e -> 2
+// i -> 3
+// o -> 4
+// u -> 5
+// For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+// Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+// For example, decode("h3 th2r2") would return "hi there".
+
+// For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+function encode(string) {
+    let alph = 'aeiou'
+      let vowels = {
+      'a': 1,
+      'e': 2,
+      'i': 3,
+      'o': 4,
+      'u': 5
+    }
+      let str = ''
+    for (let i = 0 ; i < string.length ; i++) {
+      if (alph.includes(string[i])) {
+        str += vowels[string[i]]
+      } else str += string[i]
+    } return str
+  }
+  
+  function decode(string) {
+    let nums = [1,2,3,4,5]
+      let vowels = {
+      'a': 1,
+      'e': 2,
+      'i': 3,
+      'o': 4,
+      'u': 5
+    }
+      let str = ''
+    for (let i = 0 ; i < string.length ; i++) {
+      if (nums.includes(+string[i])) {
+        str += Object.keys(vowels).find(x=>vowels[x] === +string[i])
+      } else {
+        str += string[i]
+      }
+    }
+    return str
+  }
