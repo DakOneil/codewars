46e8de128fb978ac0a9f0d93633c8677481c76d2
Dakota O'Neil <oneilkody@gmail.com>
2021-08-03 08:42:58 -0500

    add one solution

--- 8-3-21.js
+++ 8-3-21.js
@@ -101,3 +101,45 @@ function broken(x){
       else if (el==='0') return '1'
     }).join('')
   }
+
+
+
+
+
+
+// The Office V - Find a Chair
+
+// So you've found a meeting room - phew! You arrive there ready to present, and find that someone has taken one or more of the chairs!! You need to find some quick.... check all the other meeting rooms to see if all of the chairs are in use.
+
+// Your meeting room can take up to 8 chairs. need will tell you how many have been taken. You need to find that many.
+
+// Find the spare chairs from the array of meeting rooms. Each meeting room tuple will have the number of occupants as a string. Each occupant is represented by 'X'. The room tuple will also have an integer telling you how many chairs there are in the room.
+
+// You should return an array of integers that shows how many chairs you take from each room in order, up until you have the required amount.
+
+// example:
+
+// [['XXX', 3], ['XXXXX', 6], ['XXXXXX', 9], ['XXX',2]] when you need 4 chairs:
+
+// result -> [0, 1, 3] no chairs free in room 0, take 1 from room 1, take 3 from room 2. no need to consider room 3 as you have your 4 chairs already.
+
+// If you need no chairs, return "Game On". If there aren't enough spare chairs available, return "Not enough!".
+
+function meeting(x, need){
+    if (need === 0) return "Game On"
+    let takenArr = x.map(el=> {
+      if (el[1] - el[0].length > 0) return el[1] - el[0].length 
+      else return 0
+    })
+    for (let i = 0 ; i < takenArr.length ; i++) {
+      let partial = takenArr.slice(0,i).reduce((acc,c)=>acc+c,0)
+      if (partial >= need) {
+        takenArr = takenArr.slice(0,i) ;
+        break ;
+      }
+    }
+    while (takenArr.reduce((acc,c)=>acc+c,0) > need) {
+      takenArr[takenArr.length-1]--
+    }
+    return takenArr.reduce((acc,c)=>acc+c,0) < need ? 'Not enough!' : takenArr
+  }
