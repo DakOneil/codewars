80db10199c0081c156fa06caab6a37d387caab9a
Dakota O'Neil <oneilkody@gmail.com>
2021-08-03 08:07:08 -0500

    add one solution

--- 8-3-21.js
+++ 8-3-21.js
@@ -33,3 +33,51 @@ function outed(meet, boss){
     let sum = arr.reduce((acc,c)=>acc+c,0) + meet[boss]
     return sum / arr.length <= 5 ? 'Get Out Now!' : 'Nice Work Champ!' 
   } 
+
+
+
+
+
+
+//   The Office II - Boredom Score
+
+// Every now and then people in the office moves teams or departments. Depending what people are doing with their time they can become more or less boring. Time to assess the current team.
+
+// You will be provided with an object(staff) containing the staff names as keys, and the department they work in as values.
+
+// Each department has a different boredom assessment score, as follows:
+
+// accounts = 1
+// finance = 2
+// canteen = 10
+// regulation = 3
+// trading = 6
+// change = 6
+// IS = 8
+// retail = 5
+// cleaning = 4
+// pissing about = 25
+
+// Depending on the cumulative score of the team, return the appropriate sentiment:
+
+// <=80: 'kill me now'
+// < 100 & > 80: 'i can handle this'
+// 100 or over: 'party time!!'
+
+function boredom(staff){
+    let scores = {
+      'accounts': 1,
+      'finance': 2,
+      'canteen': 10,
+      'regulation': 3,
+      'trading': 6,
+      'change': 6,
+      'IS': 8,
+      'retail' :5,
+      'cleaning': 4,
+      'pissing about': 25
+    }
+    const arr = Object.values(staff)
+    let sum = arr.map(el=>scores[el]).reduce((acc,c)=>acc+c,0)
+    return sum >= 100 ? 'party time!!' : sum > 80 ? 'i can handle this' : 'kill me now'
+  }
