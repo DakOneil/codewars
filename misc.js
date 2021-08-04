9ef1529711a57390f9b426fe4df42d3da14a8924
Dakota O'Neil <oneilkody@gmail.com>
2021-08-04 09:11:04 -0500

    add one old solution

--- misc.js
+++ misc.js
@@ -1073,3 +1073,23 @@ function lastSurvivor(letters, coords) {
 
 
 
+// Halving Sum
+
+// Task
+// Given a positive integer n, calculate the following sum:
+
+// n + n/2 + n/4 + n/8 + ...
+// All elements of the sum are the results of integer division.
+
+// Example
+// 25  =>  25 + 12 + 6 + 3 + 1 = 47
+
+function halvingSum(n) {
+  let i = 1
+  let sum = 0
+  while (Math.floor(n/i) > 0) {
+    sum += Math.floor(n/i)
+    i *= 2
+  }
+return sum
+}
