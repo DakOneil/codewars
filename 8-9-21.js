7fedf16547d06bdb5a26b82d5c5165c4687282f2
Dakota O'Neil <oneilkody@gmail.com>
2021-08-09 08:16:48 -0500

    add another sol

--- 8-9-21.js
+++ 8-9-21.js
@@ -24,3 +24,54 @@ function validateHello(greetings) {
     greetings = greetings.toLowerCase()
     return hello.map(el=>greetings.includes(el)).some(x=>x)
   }
+
+
+
+
+
+// Sort the Vowels!
+
+// Sort the Vowels!
+// In this kata, we want to sort the vowels in a special format.
+
+// Task
+// Write a function which takes a input string s and return a string in the following way:
+
+   
+//                   C|                          R|
+//                   |O                          n|
+//                   D|                          d|
+//    "CODEWARS" =>  |E       "Rnd Te5T"  =>      |
+//                   W|                          T|
+//                   |A                          |e
+//                   R|                          5|
+//                   S|                          T|
+
+// Notes:
+
+// List all the Vowels on the right side of |
+// List every character except Vowels on the left side of |
+// Return every character in its original case
+// Each line is seperated with \n
+// Invalid input ( undefined / null / integer ) should return an empty string
+
+function sortVowels(s){
+    //code
+    let vowels = 'aeiou'
+    if (typeof s === "string") {
+        return s.split('').map(el=> {
+          if (vowels.includes(el.toLowerCase())) {
+            return '|' + el
+          } else {
+            return el + '|'
+          }
+        }).join('\n')
+    } else {
+      return ''
+    }
+  }
+
+
+
+
+
