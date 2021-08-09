a1c7eefe3e097a1de3741ffc7f9615665770ab98
Dakota O'Neil <oneilkody@gmail.com>
2021-08-09 08:51:48 -0500

    add solution

--- 8-9-21.js
+++ 8-9-21.js
@@ -124,3 +124,60 @@ function countRedBeads(n) {
     //your code here
     return n > 0 ? (n-1)*2 : 0
   }
+
+
+
+
+//   Prefill an Array
+
+// Create the function prefill that returns an array of n elements that all have the same value v. See if you can do this without using a loop.
+
+// You have to validate input:
+
+// v can be anything (primitive or otherwise)
+// if v is ommited, fill the array with undefined
+// if n is 0, return an empty array
+// if n is anything other than an integer or integer-formatted string (e.g. '123') that is >=0, throw a TypeError
+// When throwing a TypeError, the message should be n is invalid, where you replace n for the actual value passed to the function.
+
+// Code Examples
+
+//     prefill(3,1) --> [1,1,1]
+    
+//     prefill(2,"abc") --> ['abc','abc']
+    
+//     prefill("1", 1) --> [1]
+    
+//     prefill(3, prefill(2,'2d'))
+//       --> [['2d','2d'],['2d','2d'],['2d','2d']]
+      
+//     prefill("xyz", 1)
+//       --> throws TypeError with message "xyz is invalid"
+
+function prefill(n, v) {
+    let e = {
+      name: 'TypeError',
+      message: `${n} is invalid`
+    }
+  
+    if (typeof +n !== 'number') throw e
+    else if (n===true || n ===false) throw e
+    else if (n == 0)  return []
+    else if (+n%1!=0) throw e
+    else if (n <= 0) throw e
+    else if (!v) {
+       let arr = []
+       v = undefined
+      for (let i =1 ; i <= n ; i++) {
+        arr.push(v)
+      }
+     return arr
+    }
+    else {
+      let arr = []
+      for (let i =1 ; i <= n ; i++) {
+        arr.push(v)
+      }
+     return arr
+    }
+  }
