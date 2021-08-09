a41e5366535ec89eb7c54317d99300ffbfc54051
Dakota O'Neil <oneilkody@gmail.com>
2021-08-09 08:17:19 -0500

    add solution

--- 8-9-21.js
+++ 8-9-21.js
@@ -75,3 +75,34 @@ function sortVowels(s){
 
 
 
+
+//   Strings: swap vowels' case
+
+// Special thanks to SteffenVogel_79 for the idea.
+
+// Challenge:
+
+// Given a string, return a copy of the string with the vowels' case swapped.
+
+// For this kata, assume that vowels are in the set "aeouiAEOUI".
+
+// Example: Given a string "C is alive!", your function should return "C Is AlIvE!"
+
+// Addendum: Your solution is only required to work for the ASCII character set.
+
+// Please make sure you only swap cases for the vowels.
+
+function swapVowelCase(str) {
+    // your code here
+    let lowVow = 'aeiou'
+    let upVow = 'AEIOU'
+    return str.split('').map(el=> {
+      if (lowVow.includes(el)) {
+        return el.toUpperCase()
+      } else if (upVow.includes(el)) {
+        return el.toLowerCase()
+      } else {
+        return el
+      }
+    }).join('')
+  }
