a7fe1b6c44d5c36403eebeaa5d21c81f53f5b0fa
Dakota O'Neil <oneilkody@gmail.com>
2021-08-30 10:06:27 -0500

    add a solution

--- 8-30-21.js
+++ 8-30-21.js
@@ -65,3 +65,24 @@ function solve(s){
 function validate(password) {
     return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9]{6,}$/.test(password);
   }
+
+
+
+
+
+//Smallest value of an array
+
+// Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+
+// Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+
+// min([1,2,3,4,5], 'value') // => 1
+// min([1,2,3,4,5], 'index') // => 0
+
+function min(arr, toReturn) {
+    // TODO
+    let val = Math.min(...arr)
+    let ind = arr.indexOf(val)
+    
+    return toReturn === 'value' ? val : ind
+  }
