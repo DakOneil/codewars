942bc2ea69afaae285d2f0a4e73f40c2643faaf5
Dakota O'Neil <oneilkody@gmail.com>
2021-08-31 08:27:53 -0500

    add a sol

--- 8-31-21.js
+++ 8-31-21.js
@@ -6,3 +6,18 @@ function getMinMax(arr){
     //..
     return [Math.min(...arr), Math.max(...arr)]
   };
+
+
+
+
+
+
+
+//   Average Scores
+
+// Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+
+function average(scores) {
+    // code to calculate the average
+  return Math.round(scores.reduce((acc,c)=>acc+c,0) / scores.length)
+}
