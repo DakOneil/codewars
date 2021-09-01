507d6353f3fe2196b145f8d9d485e1d07ee4ec04
Dakota O'Neil <oneilkody@gmail.com>
2021-09-01 15:07:34 -0500

    add another solution

--- 9-1-21.js
+++ 9-1-21.js
@@ -101,3 +101,45 @@ function convertFrac(lst){
     })
     return newList
   }
+
+
+
+
+
+ //Broken sequence
+
+//  You receive some random elements as a space-delimited string. Check if the elements are part of an ascending sequence of integers starting with 1, with an increment of 1 (e.g. 1, 2, 3, 4).
+
+// Return:
+
+// 0 if the elements can form such a sequence, and no number is missing ("not broken", e.g. "1 2 4 3")
+// 1 if there are any non-numeric elements in the input ("invalid", e.g. "1 2 a")
+// n if the elements are part of such a sequence, but some numbers are missing, and n is the lowest of them ("broken", e.g. "1 2 4" or "1 5")
+// Examples
+// "1 2 3 4"  ==>  return 0, because the sequence is complete
+
+// "1 2 4 3"  ==>  return 0, because the sequence is complete (order doesn't matter)
+
+// "2 1 3 a"  ==>  return 1, because it contains a non numerical character
+
+// "1 3 2 5"  ==>  return 4, because 4 is missing from the sequence
+
+// "1 5"      ==>  return 2, because the sequence is missing 2, 3, 4 and 2 is the lowest
+
+function findMissingNumber(sequence){
+    //your code here
+    let ascendingArr = sequence.split(' ').sort((a,b)=>+a - +b)
+  
+    if (sequence.length === 0) { return 0 }
+    else if (!/^[0-9\s]*$/.test(sequence)) { return 1 }
+    else if (ascendingArr.length == ascendingArr.reverse()[0]) { return 0 }
+    else {
+      ascendingArr.reverse()
+      for (let i = 0 ; i <= ascendingArr.length ; i++) {
+        if (+ascendingArr[i] !== i+1) {
+          return i+1
+        }
+      }
+    } 
+    return 0 
+  }
