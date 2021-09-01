492e1663de479a40fc600a0a8da7cc830d502918
Dakota O'Neil <oneilkody@gmail.com>
2021-09-01 13:42:01 -0500

    add another 5 kyu sol

--- 9-1-21.js
+++ 9-1-21.js
@@ -37,3 +37,67 @@ var isPP = function(n){
    
    return null
  }
+
+
+
+
+
+ //Common Denominators
+
+//  Common denominators
+
+// You will have a list of rationals in the form
+
+// { {numer_1, denom_1} , ... {numer_n, denom_n} } 
+// or
+// [ [numer_1, denom_1] , ... [numer_n, denom_n] ] 
+// or
+// [ (numer_1, denom_1) , ... (numer_n, denom_n) ] 
+// where all numbers are positive ints. You have to produce a result in the form:
+
+// (N_1, D) ... (N_n, D) 
+// or
+// [ [N_1, D] ... [N_n, D] ] 
+// or
+// [ (N_1', D) , ... (N_n, D) ] 
+// or
+// {{N_1, D} ... {N_n, D}} 
+// or
+// "(N_1, D) ... (N_n, D)"
+// depending on the language (See Example tests) in which D is as small as possible and
+
+// N_1/D == numer_1/denom_1 ... N_n/D == numer_n,/denom_n.
+// Example:
+// convertFracs [(1, 2), (1, 3), (1, 4)] `shouldBe` [(6, 12), (4, 12), (3, 12)]
+// Note:
+// Due to the fact that the first translations were written long ago - more than 6 years - these first translations have only irreducible fractions.
+
+// Newer translations have some reducible fractions. To be on the safe side it is better to do a bit more work by simplifying fractions even if they don't have to be.
+
+function convertFrac(lst){
+    //Your code here
+    let denoms = []
+    lst.forEach(el=>{
+      denoms.push(el[1])
+    })
+    let sort = denoms.sort((a,b)=>b-a)
+    let big = sort[0]
+    let lcd = 0 
+    let i = 1
+    while (lcd === 0) {
+      let check = denoms.map(el=> (big * i)%el === 0)
+      if (check.every(x=>x===true)) {
+        lcd = i*big
+      }
+      i++
+    }  
+    let numers = []
+    lst.forEach(el=> {
+      numers.push(Math.round((el[0] / el[1]) * lcd))
+    })
+    let newList = ''
+    numers.forEach(el => {
+      newList += `(${el},${lcd})`
+    })
+    return newList
+  }
