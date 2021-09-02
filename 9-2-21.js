b08a173fadaf79c81780438689589cc700471b91
Dakota O'Neil <oneilkody@gmail.com>
2021-09-02 08:53:21 -0500

    add a solution

--- 9-2-21.js
+++ 9-2-21.js
@@ -39,3 +39,36 @@ function chirp(n) {
     else if(n==1) return 'chirp'
     else return chirp(n-1) + '-' + chirp(0)
   }
+
+
+
+
+
+//Fibonacci Reloaded
+
+// And here is Fibonacci again. This time we want to go one step further. Our fib() function must be faster! Can you do it?
+
+// In case you don't know, what the Fibonacci number is:
+
+// The nth Fibonacci number is defined by the sum of the two previous Fibonacci numbers. In our case: fib(1) := 0 and fib(2) := 1. With these initial values you should be able to calculate each following Fibonacci number.
+
+// Examples:
+
+// fib(1) // === 0
+// fib(2) // === 1
+// fib(3) // === 1
+// fib(4) // === 2
+// fib(5) // === 3
+
+function fib(num) {
+    var a = 0, b = 1, temp;
+    
+      while (num >= 1){
+        temp = a;
+        a = a + b;
+        b = temp;
+        num--;
+      }
+    
+      return b;
+    }
