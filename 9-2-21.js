70b9e9565b5fa1e1215f84a9e74a1ab17221e332
Dakota O'Neil <oneilkody@gmail.com>
2021-09-02 08:35:14 -0500

    add recursive solution

--- 9-2-21.js
+++ 9-2-21.js
@@ -20,3 +20,22 @@ const fibonacci = n => {
     if (n==0 || n==1) return n
     else return fibonacci(n-1) + fibonacci(n-2)
   };
+
+
+
+
+
+  //Recursive Ninja
+
+//   Ninjas frequently need to signal each other in code, often employing natural sounds as a cover. We’re going to give our ninja the ability to chirp like a cricket, with the number of chirps encoding different messages.
+
+// Implement a "recursive function" that takes one parameter (n) and outputs a string i.e.
+
+//   chirp(4);
+//   //output would be chirp-chirp-chirp-chirp.
+
+function chirp(n) {
+    if (n==0) return 'chirp'
+    else if(n==1) return 'chirp'
+    else return chirp(n-1) + '-' + chirp(0)
+  }
